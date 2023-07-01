1631a2c2140ac3c9026fa7015ee010ddcbb80a2a
Eamonn Hughes <efohughes@gmail.com>
2023-07-01 11:17:55 -0400

    three

--- core/src/org/eamonn/wfc/scenes/Home.scala
+++ core/src/org/eamonn/wfc/scenes/Home.scala
@@ -10,10 +10,12 @@ import scala.util.Random
 
 class Home extends Scene {
 
-  var grid: Array[gridItem] = Array.fill(dimensions * dimensions)(gridItem(collapsed = false, List(BLANK, DOWN, LEFT, RIGHT, UP)))
+  var grid: Array[gridItem] = Array.fill(dimensions * dimensions)(
+    gridItem(collapsed = false, List(BLANK, DOWN, LEFT, RIGHT, UP))
+  )
 
   override def init(): InputAdapter = {
-      new HomeControl(this)
+    new HomeControl(this)
 
   }
 
@@ -23,30 +25,50 @@ class Home extends Scene {
 
   //pick cell with least entropy
   def collapseLeast(): Unit = {
-    if(grid.exists(_.collapsed == false)){
-    val leastLength = grid.filter(_.collapsed == false).map(_.options.length).min
-    val least = grid.filter(cell => {cell.options.length == leastLength && !cell.collapsed})
+    if (grid.exists(_.collapsed == false)) {
+      val leastLength =
+        grid.filter(_.collapsed == false).map(_.options.length).min
+      val least = grid.filter(cell =>
+        cell.options.length == leastLength && !cell.collapsed
+      )
 
-    val picked = least(Random.nextInt(least.length))
-    grid(grid.indexOf(picked)).collapsed = true
-    grid(grid.indexOf(picked)).options = List(picked.options(Random.nextInt(picked.options.length)))
+      val picked = least(Random.nextInt(least.length))
+      picked.collapsed = true
+      picked.options = List(
+        picked.options(Random.nextInt(picked.options.length))
+      )
     }
   }
 
   override def render(batch: PolygonSpriteBatch): Unit = {
-    for(x <- 0 until dimensions) {
-      for(y <- 0 until dimensions){
-        var cell = grid(x + y*dimensions)
+    for (x <- 0 until dimensions) {
+      for (y <- 0 until dimensions) {
+        var cell = grid(x + y * dimensions)
         batch.setColor(Color.WHITE)
-        if(cell.collapsed){
+        if (cell.collapsed) {
           var index = cell.options.head
-          batch.draw(tiles(index), x * screenUnit, y * screenUnit, screenUnit, screenUnit)
+          batch.draw(
+            tiles(index),
+            x * screenUnit,
+            y * screenUnit,
+            screenUnit,
+            screenUnit
+          )
         } else {
-          batch.draw(Wfc.unChosen, x* screenUnit, y * screenUnit, screenUnit, screenUnit)
+          batch.draw(
+            Wfc.unChosen,
+            x * screenUnit,
+            y * screenUnit,
+            screenUnit,
+            screenUnit
+          )
         }
       }
     }
   }
 }
 
-case class gridItem(var collapsed: Boolean = false, var options: List[Int] = List(BLANK, DOWN, LEFT, RIGHT, UP))
+case class gridItem(
+    var collapsed: Boolean = false,
+    var options: List[Int] = List(BLANK, DOWN, LEFT, RIGHT, UP)
+)

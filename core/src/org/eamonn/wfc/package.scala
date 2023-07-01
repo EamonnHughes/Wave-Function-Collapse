4545502f4322fbb4fffc3cf8ca02678ecb06f3a8
Eamonn Hughes <efohughes@gmail.com>
2023-07-01 10:05:06 -0400

    initial commit

--- core/src/org/eamonn/wfc/package.scala
+++ core/src/org/eamonn/wfc/package.scala
@@ -3,7 +3,6 @@ package org.eamonn
 import com.badlogic.gdx.Gdx.input
 import com.badlogic.gdx.Input.Peripheral
 import com.badlogic.gdx.graphics.Color
-import org.eamonn.wfc.util.Vec2
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
